87f9ad3c6f75494cc7557554340db93e383da5b1
jujubazw <ajeaanalu@gmail.com>
2026-07-01 20:57:22 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -15,6 +15,16 @@ public class Livro implements Serializable{
   private List<Exemplar> exemplares = new ArrayList<>();
   private List<Autor> autores = new ArrayList<>();
   private List<Categoria> categorias = new ArrayList<>();
+  private List<Reserva> reservas = new ArrayList<>();
 
   public Livro(String isbn, String titulo, int anoPublicacao, int edicao, Editora editora){
-    
+    this.isbn = isbn;
+    this.titulo = titulo;
+    this.anoPublicacao = anoPublicacao;
+    this.edicao = edicao;
+    this.editora = editora;
+  }
+
+public boolean buscarDisponibilidade(){
+  return exemplares.stream().anyMatch(Exemplar::verificarDisponibilidade);
+}
