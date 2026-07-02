c1a6d1f4e2cc7eebc901c582429c1c8b577e859f
jujubazw <ajeaanalu@gmail.com>
2026-07-01 22:16:40 -0300

    Create Exemplar.java

---
+++ Exemplar.java
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+package mochila;
+
+import java.time.LocalDate;
+import java.io.Serializable;
+
+public class Exemplar implements Serializable{
+  
+  private String codigoExemplar;
+  private StatusExemplar status;
+  private LocalDate dataAquisicao;
+  
+  public Exemplar(String codigoExemplar, LocalDate dataAquisicao){
+    this.codigoExemplar = codigoExemplar;
+    this.dataAquisicao = dataAquisicao;
+    this.status = StatusExemplar.DISPONIVEL;
+  }
+
+  public boolean verificarDisponibilidade(){
+    return status == StatusExemplar.DISPONIVEL;
+  }
+
+  public void atualizarStatus(StatusExemplar status){
+    this.status = status;
+  }
+
+  public String getCodigoExemplar(){
+    return codigoExemplar;
+  }
+
+  public void setCodigoExemplar(String codigoExemplar){
+    this.codigoExemplar = codigoExemplar;
+  }
+
+  
+
+}
