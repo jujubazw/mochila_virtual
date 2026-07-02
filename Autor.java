e6df2bc3392cf5ad396278169a7425c754abcf90
jujubazw <ajeaanalu@gmail.com>
2026-07-02 02:01:35 -0300

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -3,6 +3,7 @@ package mochila;
 import java.io.Serializable;
 
 public class Autor implements Serializable{
+  
   private int id;
   private String nome;
   private String nacionalidade;
@@ -12,5 +13,24 @@ public class Autor implements Serializable{
     this.nome = nome;
     this.nacionalidade = nacionalidade;
   }
-  
+
+  public int getId(){
+    return id;
+  }
+
+  public String getNome(){
+    return nome;
+  }
+
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public String getNacionalidade(){
+    return nacionalidade;
+  }
+
+  public void setNacionalidade(String nacionalidade){
+    this.nacionalidade = nacionalidade;
+  }
 }
