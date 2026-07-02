81bb163ced5ebd4dbc6d18a006ef4c0548162c84
jujubazw <ajeaanalu@gmail.com>
2026-07-02 02:21:15 -0300

    Update Categoria.java

--- Categoria.java
+++ Categoria.java
@@ -1,3 +1,18 @@
 
 
 package mochila;
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+  
+  private int id;
+  private String nome;
+  private String descricao;
+
+  public Categoria(int id, String nome, String descricao){
+    this.id = id;
+    this.nome = nome;
+    this.descricao = descricao;
+  }
+  
+}
